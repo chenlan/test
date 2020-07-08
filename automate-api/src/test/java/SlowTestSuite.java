21b82d0bbb4241b3897cf0390e22591f0081b873
chenlan <304242276@qq.com>
2020-07-08 16:11:40 +0800

    接口测试调整

---
+++ automate-api/src/test/java/SlowTestSuite.java
@@ -0,0 +1,15 @@
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.runner.RunWith;
+import service.user.UserPoTest;
+
+@RunWith(JUnitPlatform.class)
+@SuiteDisplayName("JUnit Platform Suite Demo")
+@SelectClasses({
+        UserPoTest.class
+})
+@IncludeTags({"slow"})
+public class SlowTestSuite {
+}
